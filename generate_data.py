1cef0f004f89adb8096df2869449526f9aa64635
Dharma Ranganathan <dharmaqubla369@gmail.com>
2025-11-22 11:16:51 +0530

    Delete generate_data.py

--- generate_data.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from sklearn.datasets import make_blobs
-
-def generate_data(n=300, centers=4):
-    X, labels = make_blobs(n_samples=n, centers=centers, cluster_std=1.2, random_state=42)
-    return X, labels
